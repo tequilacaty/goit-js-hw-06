2c915845808916952150d049a9de22ae2973f1a2
Svitlana <s.krutelyova@gmail.com>
2023-07-31 18:42:20 +0300

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyEl = document.querySelector("body");
+const buttonEl = document.querySelector(".change-color");
+const colorNumberEl = document.querySelector(".color");
+
+buttonEl.addEventListener("click", changeColor);
+
+function changeColor() {
+  const CurrentColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = CurrentColor;
+  colorNumberEl.textContent = CurrentColor;
+}
