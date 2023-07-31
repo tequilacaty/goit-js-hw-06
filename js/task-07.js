b6f4d6cb10727122539eae8071bc7755c72afbe9
Svitlana <s.krutelyova@gmail.com>
2023-07-31 14:08:23 +0300

    task 7

--- js/task-07.js
+++ js/task-07.js
@@ -3,7 +3,6 @@ const textEl = document.querySelector("span#text");
 fontSize.addEventListener("input", changeTextSize);
 
 function changeTextSize() {
-  // console.log(fontSize.value);
   textEl.style.fontSize = fontSize.value + "px";
 }
 changeTextSize();
