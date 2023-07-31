5e2440be4c50b9557450e1616e15548a8fbab52a
Svitlana <s.krutelyova@gmail.com>
2023-07-31 17:18:04 +0300

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Всі поля повинні бути заповнені");
+  }
+
+  const formData = {
+    email: email.value,
+    password: password.value,
+  };
+  console.log(formData);
+
+  event.currentTarget.reset();
+}
